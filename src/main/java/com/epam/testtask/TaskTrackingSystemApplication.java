2780c2d64b6d8c103ba45b014b62b27c247f80f9
Il'ya Suhov <menace1290@yandex.ru>
2020-07-03 17:41:40 +0300

    -fixed user filter -dynamic path to db

--- src/main/java/com/epam/testtask/TaskTrackingSystemApplication.java
+++ src/main/java/com/epam/testtask/TaskTrackingSystemApplication.java
@@ -9,11 +9,23 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class TaskTrackingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTrackingSystemApplication.class, args);
+		openHomePage();
+	}
+
+	private static void openHomePage() {
+		Runtime rt = Runtime.getRuntime();
+		try {
+			rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 //	@Configuration
